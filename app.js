87566c904d09453981838f69d46d6c3799a7cac0
robae2821 <89492065+robae2821@users.noreply.github.com>
2021-12-27 19:26:09 -0700

    change index

--- app.js
+++ app.js
@@ -8,3 +8,18 @@
 //     }
 //     document.getElementById("comment").innerHTML = txt;
 // }
+
+var myCarousel = document.querySelector('.carousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: false,
+    wrap: false
+  })
+
+console.log('javascript loaded');
+
+myCarousel.addEventListener('slid.bs.carousel', function () {
+  console.log('The slide has finished sliding. It slid.!')
+  var currentImage = document.querySelector('.active').children[0]
+  console.log(`the current alt is: ${currentImage.alt}`)
+  console.log(`the current source is: ${currentImage.src}`)
+})
